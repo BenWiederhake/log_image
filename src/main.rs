1998acf4627c6b25af239f549dde3b8a307f4cbd
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2019-12-02 01:46:04 +0100

    Nicer output

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ const UPPER: u32 = 0xFFFFFFFFu32;
 fn main() {
     let mut seen = BTreeMap::<u32, u32>::new();
     for a in 0u32..=UPPER {
-        if 0 == a & 0x003FFFFF {
+        if 0 == a & 0x00FFFFFF {
             eprintln!("--- iter 0x{:08x} (0x{:08x} elems)", a, seen.len());
         }
         let b = f32::from_bits(a).ln().to_bits();
@@ -13,6 +13,7 @@ fn main() {
     }
     eprintln!("Ran for {0} (0x{0:08x}) iterations, saw only {1} (0x{1:08x}) different values", UPPER, seen.len());
     let mut histhist = BtreeMap::<u32, u32>::new();
+    eprintln!("Grouping ...");
     for (k, v) in seen {
         *histhist.entry(v).or_default() += 1;
     }
