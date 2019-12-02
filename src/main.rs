4de30976ce30eaf691998bebb513cdb01bc654b9
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2019-12-02 01:28:59 +0100

    Better feedback

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::collections::BTreeMap;
 
 const UPPER: u32 = 0xFFFFFFFFu32;
-//const UPPER: u32 = 0xFFFFFFFFu32;
 
 fn main() {
     let mut seen = BTreeMap::<u32, u32>::new();
     for a in 0u32..=UPPER {
         if 0 == a & 0x003FFFFF {
-            eprintln!("--- iter 0x{:08x} ({} elems)", a, seen.len());
+            eprintln!("--- iter 0x{:08x} (0x{:08x} elems)", a, seen.len());
         }
         let b = f32::from_bits(a).ln().to_bits();
         *seen.entry(b).or_default() += 1;
     }
     eprintln!("Ran for {0} (0x{0:08x}) iterations, saw only {1} (0x{1:08x}) different values", UPPER, seen.len());
     for (k, v) in seen {
-        println!("{:08x}: {} times", k, v);
+        if v != 1 {
+            println!("{:08x}: {} times", k, v);
+        }
     }
 }
